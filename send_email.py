f90f7799727ec72413ec63660fcfae4767368754
dm-alekseev <alekseev@stw.by>
2025-04-14 19:37:05 +0300

    nev test commit

--- send_email.py
+++ send_email.py
@@ -1,4 +1,4 @@
-import smtplibi
+import smtplib
 import sys
 from email.mime.text import MIMEText
 from email.message import EmailMessage
@@ -12,7 +12,7 @@ msg = EmailMessage()
 msg['Subject'] = 'Новый коммит!'
 msg['From'] = sender_email
 msg['To'] = recipient_email
-msg.set_content(commit_msg)
+msg.set_content[''] = 'commit_msg'
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
     server.login(sender_email, sender_password)
