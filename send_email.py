42089e6137b2d008f85096b35dc0360fbcd2674a
dm-alekseev <alekseev@stw.by>
2025-04-14 19:20:40 +0300

    test

--- send_email.py
+++ send_email.py
@@ -1,4 +1,5 @@
 import smtplib
+from email.mime.text import MIMEText
 from email.message import EmailMessage
 from credentials import *
 
@@ -6,10 +7,10 @@ from credentials import *
 msg = EmailMessage()
 msg['Subject'] = 'Новый коммит!'
 msg['From'] = sender_email
-msg['To'] = 'a@stw.by'
+msg['To'] = recipient_email
 msg.set_content(f'Сделан коммит с текстом:')
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
-    server.login('puhxxx@gmail.com', 'caok opfy tbqa jjod')
+    server.login(sender_email, sender_password)
     server.send_message(msg)
 
