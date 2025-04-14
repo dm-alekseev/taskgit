6498a9ec0303e445e84c140afe0f8c680388ef77
dm-alekseev <alekseev@stw.by>
2025-04-14 20:07:08 +0300

    new test commit

--- send_email.py
+++ send_email.py
@@ -4,15 +4,16 @@ from email.mime.text import MIMEText
 from email.message import EmailMessage
 from credentials import *
 
-
+branch_name = sys.stdin.read().strip()
 commit_msg = sys.stdin.read().strip()
+print(branch_name)
 print(commit_msg)
 
 msg = EmailMessage()
 msg['Subject'] = 'New commit'
 msg['From'] = sender_email
 msg['To'] = recipient_email
-msg.set_content(f'{commit_msg}')
+msg.set_content(f'Branch: {branch_name} , {commit_msg}')
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
     server.login(sender_email, sender_password)
